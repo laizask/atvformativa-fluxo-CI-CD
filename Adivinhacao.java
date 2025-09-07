96de7bc3a50c8f7d98b2b3381484e173d73ca9ce
laiza barbosa <146351860+laizask@users.noreply.github.com>
2025-09-07 13:18:15 -0300

    Lógica do jogo

--- Adivinhacao.java
+++ Adivinhacao.java
@@ -1,5 +1,11 @@
+import java.util.random.RandomGenerator;
+
 public class Adivinhacao {
     public static void main(String[] args) {
         System.out.println("Seja bem-vindo ao jogo de Adivinhação! O objetivo é adivinhar o número escolhido pelo computador.");
-    }
+
+    int numeroMaximo = 30;
+    int numeroMinimo = 1;
+    int numeroSecreto = numeroMinimo + (int)(Math.random() * (numeroMaximo - numeroMinimo + 1));
+}
 }
