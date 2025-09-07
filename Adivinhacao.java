7fc85e9ebe9809a2ef1d3c0ea03805d73ff3f8c9
laiza barbosa <146351860+laizask@users.noreply.github.com>
2025-09-07 13:10:32 -0300

    Mensagem inicial

--- Adivinhacao.java
+++ Adivinhacao.java
@@ -1,3 +1,5 @@
 public class Adivinhacao {
-
+    public static void main(String[] args) {
+        System.out.println("Seja bem-vindo ao jogo de Adivinhação! O objetivo é adivinhar o número escolhido pelo computador.");
+    }
 }
